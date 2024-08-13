682601f67aafdd0af859064695318fe7702182af
Abdulghani Ha <harranAbdulghani@gmail.com>
2024-08-13 16:45:45 +0300

    [add] smtp

--- superset/config.py
+++ superset/config.py
@@ -1358,13 +1358,14 @@ CONFIG_PATH_ENV_VAR = "SUPERSET_CONFIG_PATH"
 FLASK_APP_MUTATOR = None
 
 # smtp server configuration
-SMTP_HOST = "localhost"
+EMAIL_NOTIFICATIONS = True  # all the emails are sent using dryrun
+SMTP_HOST = os.getenv("SMTP_HOST")
 SMTP_STARTTLS = True
 SMTP_SSL = False
-SMTP_USER = "superset"
-SMTP_PORT = 25
-SMTP_PASSWORD = "superset"  # noqa: S105
-SMTP_MAIL_FROM = "superset@superset.com"
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PORT = os.getenv("SMTP_PORT")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+SMTP_MAIL_FROM = os.getenv("SMTP_MAIL_FROM")
 # If True creates a default SSL context with ssl.Purpose.CLIENT_AUTH using the
 # default system root CA certificates.
 SMTP_SSL_SERVER_AUTH = False
